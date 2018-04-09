1b5d3ff5811f003e45b465b945ddca57f9325b3d
Blake La Pierre <blakelapierre@gmail.com>
2018-04-09 18:13:17 -0400

    more pools

--- www/src/pools.ts
+++ www/src/pools.ts
@@ -2,5 +2,16 @@ export default [
   'https://pool.ipbc.io:8119/live_stats',
   'https://support.ipbc.io:8119/live_stats',
   'https://cryptoknight.cc/rpc/ipbc/live_stats',
-  'http://54.148.101.99:8008/live_stats'
+  'http://54.148.101.99:8008/live_stats',
+  'https://ipbc-pool.network:8443/live_stats',
+  'http://ipbc-us.labbinarymining.com:8317/live_stats',
+  'http://ipbccoin.newpool.pw:8117/live_stats',
+  'http://ipbc.pool.ahka.net:8117/stats',
+  'http://54.148.101.99:8008/live_stats',
+  'http://ipbc.pool.ahka.net:8117/stats',
+  'https://ipbc.dreampool.info/api/live_stats',
+  'http://btcn.pool.pilbeams.net:8117/stats',
+  'https://etn.spacepools.org/api/cf_stats',
+  'http://ipbc.pool.ahka.net:8117/stats',
+  'https://ipbc.minez.ro/live_stats'
 ];
